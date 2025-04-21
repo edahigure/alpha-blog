173a6216aee6d22ac8e26af49a36a0ae295afba7
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2025-04-21 15:02:59 -0600

    deploy in render 5

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-
-# Exit on error
 set -o errexit
 
-bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
+# Instala dependencias
+bundle install --without development test
 
-# If you have a paid instance type, we recommend moving
-# database migrations like this one from the build command
-# to the pre-deploy command:
-bin/rails db:migrate
+# Precompila assets
+bundle exec rails assets:precompile
+
+# Ejecuta migraciones
+bundle exec rails db:migrate
