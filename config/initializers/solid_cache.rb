eea6e8c00b9ae1121deeabb0ac108a9dfe91be70
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2025-04-21 21:27:39 -0600

    commit 12

--- config/initializers/solid_cache.rb
+++ config/initializers/solid_cache.rb
@@ -1,3 +1,11 @@
 SolidCache.configure do |config|
-  config.database = :primary # Use main database
+  # For production (including Render)
+  if ENV['RENDER'] || Rails.env.production?
+    config.database = :primary  # Use main database
+  end
+
+  # For development/test
+  if Rails.env.development? || Rails.env.test?
+    config.store = :memory_store
+  end
 end
