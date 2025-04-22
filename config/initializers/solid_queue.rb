1c82b482627301f6def13de3f5b425bcf8a17b90
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2025-04-21 20:47:51 -0600

    commit 10

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,2 +1,10 @@
-# Share main database connection
-ActiveRecord::Base.configurations[:queue] = ActiveRecord::Base.configurations[:production]
+# For Rails 8.0.2+ configuration
+if defined?(ActiveRecord::Base)
+  queue_config = ActiveRecord::Base.configurations.configs_for(env_name: Rails.env, name: "primary").configuration_hash
+  ActiveRecord::Base.configurations.configurations << ActiveRecord::DatabaseConfigurations::HashConfig.new(Rails.env, "queue", queue_config)
+end
+
+# Optional development/test configuration
+if Rails.env.development? || Rails.env.test?
+  SolidQueue.mode = :inline
+end
